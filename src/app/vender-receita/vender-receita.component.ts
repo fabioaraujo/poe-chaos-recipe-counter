3150cd97c599c2879c4c18ce9cf5fc46aceabf72
Fabio Araujo <fabioaraujods@gmail.com>
2020-06-24 10:29:55 -0300

    incluído contador de receitas para venda

--- src/app/vender-receita/vender-receita.component.ts
+++ src/app/vender-receita/vender-receita.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VenderReceitaComponent implements OnInit {
   chaos: number;
+  receitasDisponiveisParaVenda: number;
 
   constructor(private itemCounter: ItemCounterService) { }
 
@@ -16,12 +17,33 @@ export class VenderReceitaComponent implements OnInit {
     if (isNaN(this.chaos)) {
       this.chaos = 0;
     }
+    this.atualizaReceitasDisponiveis();
+    this.itemCounter.watchStorage().subscribe((data:string) => {
+      this.atualizaReceitasDisponiveis();
+    });
+  }
+
+  atualizaReceitasDisponiveis() {
+    let quantidadeMaxima = -1;
+    for (const i in this.itemCounter.itens) {
+      const item = this.itemCounter.itens[i];
+      let quantidadeItens = this.itemCounter.getQuantidade(item);
+      if ("Ring" === item) {
+        quantidadeItens = Math.floor(quantidadeItens / 2);
+      }
+     if (quantidadeMaxima == -1 || quantidadeMaxima > quantidadeItens){
+        quantidadeMaxima = quantidadeItens;
+     }
+    }
+    this.receitasDisponiveisParaVenda = quantidadeMaxima;
   }
 
   venderReceita(){
-    this.itemCounter.venderReceita();
-    this.chaos += 2;
-    localStorage.setItem('chaos_orbs', this.chaos.toString());
+    if (this.receitasDisponiveisParaVenda > 0) {
+      this.itemCounter.venderReceita();
+      this.chaos += 2;
+      localStorage.setItem('chaos_orbs', this.chaos.toString());
+    }
   }
 
 }
